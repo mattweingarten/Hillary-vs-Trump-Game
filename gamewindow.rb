d56e10cb73fe2495b78596c6fa069588cfe29c57
Matthew Weingarten <matt@weingarten.org>
2016-12-14 21:49:41 -0800

    Adds sound an more responsive shooting

--- gamewindow.rb
+++ gamewindow.rb
@@ -2,12 +2,15 @@ class GameWindow < Gosu::Window
   module ZOrder
     Background, Stars, Player, UI = *0..3
   end
+  attr_reader :beap
+
   def initialize
     super 640, 480, :fullscreen => true
     self.caption = "My Game"
     @background_image = Gosu::Image.new("background_image.jpg", :tileable => true)
     @player_1 = Player.new("figure.png") #trump
     @player_2 = Player.new("figure_2.png") #hilary
+    @beep = Gosu::Sample.new("song.mp3")
     @player_2.warp(320, 400)
     @player_1.warp(320,50)
     @projectile = nil
@@ -15,21 +18,27 @@ class GameWindow < Gosu::Window
     @stars = Array.new
     @time = Array.new
     @time_arrays = Array.new
+    @hits = 0
+  end
+  def play_song
+    @beep.play
   end
 
   def update
+
       second = Time.now.sec
       @time.push(second)
       @stars.each do |star|
       if star.x > @player_2.x_hit_left && star.x < @player_2.x_hit_right && star.y > @player_2.y_hit_up && star.y < @player_2.y_hit_down
-        p "hit! ===================================  1 :  0  ======================================================"
+        @hits += 1
+        p "hit! ===================================  #{@hits} :  0  ======================================================"
         @player_2.image = Gosu::Image.new("explosion.png")
 
       end
     end
 
     if Gosu::button_down? Gosu::KbSpace then
-      if @time.last >= @time[-2] + 1
+      if @time.last >= @time[-10] + 1
       @stars.push(Star.new(@star_anim, @player_1.x))
       end
     end
@@ -57,7 +66,7 @@ class GameWindow < Gosu::Window
       end
     end
     if Gosu::button_down? Gosu::KbSpace then
-       @player_1.image = Gosu::Image.new("evil_trump.jpg")
+       @player_1.image = Gosu::Image.new("evil_trump.png")
        @projectile = Player.new("projectile.png")
        @projectile.warp(320,50)
     end
--- main.rb
+++ main.rb
@@ -3,5 +3,6 @@ require_relative 'player'
 require_relative 'gamewindow'
 require_relative 'star'
 window = GameWindow.new
+window.play_song
 window.show
 window.draw
